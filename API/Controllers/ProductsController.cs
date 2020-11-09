f3997ef73b7d93c4f56736b216c2007aa9ba9a23
Veso <Ambroza8888@gmail.com>
2020-11-09 03:25:12 -0800

    spacing

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -45,6 +45,7 @@ namespace API.Controllers
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
             var product = await _productsRepo.GetEntityWithSpec(spec);
+
             return Ok(product);
         }
 
--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -6,6 +6,10 @@ namespace Core.Specifications
 {
     public class BaseSpecification<T> : ISpecification<T>
     {
+
+        public Expression<Func<T, bool>> Criteria {get; }
+
+        public List<Expression<Func<T, object>>> Includes {get; } = new List<Expression<Func<T, object>>>();
         public BaseSpecification()
         {
         }
@@ -14,10 +18,6 @@ namespace Core.Specifications
         {
             Criteria = criteria;
         }
-
-        public Expression<Func<T, bool>> Criteria {get; }
-
-        public List<Expression<Func<T, object>>> Includes {get; } = new List<Expression<Func<T, object>>>();
 
         protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
