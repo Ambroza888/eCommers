5c94cbe12f504d393555c590429eb4da363bfa0c
Veso <Ambroza8888@gmail.com>
2020-12-13 03:23:37 -0800

    added account components and interfaces SPA.

--- API/Extensions/UserManagerExtentions.cs
+++ API/Extensions/UserManagerExtentions.cs
@@ -9,13 +9,13 @@ namespace API.Extensions
 {
     public static class UserManagerExtentions
     {
-        public static async Task<AppUser> FindUserByClaimsPrincipalWithAddressAsync(this UserManager<AppUser> input, ClaimsPrincipal user)
+        public static async Task<AppUser> FindUserByClaimsPrincipalWithAddressAsync(this UserManager<AppUser> _userManager, ClaimsPrincipal user)
         {
 
             // ClaimsPrincipal = HttpContext?.User?
             var email = user?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
 
-            return await input.Users.Include(x => x.Address).FirstOrDefaultAsync(x => x.Email == email);
+            return await _userManager.Users.Include(x => x.Address).FirstOrDefaultAsync(x => x.Email == email);
         }
 
         public static async Task<AppUser> FindUserByEmailFromClaimsPrincipal(this UserManager<AppUser> input, ClaimsPrincipal user)
