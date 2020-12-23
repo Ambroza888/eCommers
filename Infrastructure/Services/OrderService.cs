691246098f3b89ba2edb623e8200bd08ab56d4ed
Veso <Ambroza8888@gmail.com>
2020-12-23 10:06:40 -0800

    working on the sudocode for creatingOrder.

--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Core.Entities;
 using Core.Entities.OrderAggregate;
 using Core.Interfaces;
 
@@ -7,10 +8,33 @@ namespace Infrastructure.Services
 {
   public class OrderService : IOrderService
   {
-    public Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string basketId, Address shippingAddress)
+    private readonly IGenericRepository<Order> _orderRepo;
+    private readonly IGenericRepository<DeliveryMethod> _dmRepo;
+    private readonly IGenericRepository<Product> _productRepo;
+    private readonly IBasketRepository _basketRepo;
+    public OrderService(IGenericRepository<Order> orderRepo, IGenericRepository<DeliveryMethod> dmRepo, IGenericRepository<Product> productRepo, IBasketRepository basketRepo)
+    {
+      _basketRepo = basketRepo;
+      _productRepo = productRepo;
+      _dmRepo = dmRepo;
+      _orderRepo = orderRepo;
+    }
+
+    public async Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string basketId, Address shippingAddress)
     {
       // get basket from basket repo
+      var basket = await _basketRepo.GetBasketAsync(basketId);
+
       // get items from the product repo
+      var items = new List<OrderItem>();
+      foreach (var item in basket.Items)
+      {
+        var productItem = await _productRepo.GetByIdAsync(item.Id);
+        var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name, productItem.PictureUrl);
+        var orderItem = new OrderItem(itemOrdered, productItem.Price, item.Quantity);
+        items.Add(orderItem);
+      }
+
       // get delivery method from repo
       // calc subtotal
       // create order
